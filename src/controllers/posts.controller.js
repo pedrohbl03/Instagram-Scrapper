7bc2ac9c34510f079792c30f90e6e5728bb2011a
Pedro Lima <pedrohblima03@gmail.com>
2022-03-24 13:26:47 -0300

    Post route modify

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const { Posts } = require('../global/globalVariables')
+const { Posts } = require('../global/globalVariables');
 
 const getPostsController = async (req, res) => {
   res.send(JSON.stringify(Posts, null, 4));
