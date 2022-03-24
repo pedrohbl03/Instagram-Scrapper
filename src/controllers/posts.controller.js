33602ebe6b1da014e4e12df181fb1ce608c75419
Pedro Lima <pedrohblima03@gmail.com>
2022-03-24 20:42:35 -0300

    Add status in response of getPosts

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const { Posts } = require('../global/globalVariables');
 
 const getPostsController = async (req, res) => {
-  res.send(JSON.stringify(Posts, null, 4));
+  res.status(httpStatus.OK).send(JSON.stringify(Posts, null, 4));
 }
 
 module.exports = {
