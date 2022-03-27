78e1babaf7e34fabe6b6511ce19708ce9aae478a
Pedro Lima <pedrohblima03@gmail.com>
2022-03-27 17:20:23 -0300

    Add setposts router

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,10 +1,18 @@
 const httpStatus = require('http-status');
-const { Posts } = require('../global/globalVariables');
+const { Posts, User } = require('../global/globalVariables');
+const { getPagePosts } = require('../services/posts.service');
 
 const getPostsController = async (req, res) => {
   res.status(httpStatus.OK).send(JSON.stringify(Posts, null, 4));
 }
 
+const setPostsController = async (req, res) => {
+  let { user, hashtag } = User
+  await getPagePosts(user, hashtag);
+  res.status(httpStatus.CREATED).send('POSTS SETADOS.');
+}
+
 module.exports = {
-  getPostsController
+  getPostsController,
+  setPostsController
 }
--- src/routes/v1/posts.router.js
+++ src/routes/v1/posts.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getPostsController } = require('../../controllers/posts.controller');
+const { getPostsController, setPostsController } = require('../../controllers/posts.controller');
 
 const router = express.Router();
 
 router.get('/get', (req, res) => getPostsController(req, res))
+router.get('/set', (req, res) => setPostsController(req, res))
 
 module.exports = router;
